94810135af837fafe8f55480b04650b10538676a
Ani Ghasabyan <gasabyanani@gmail.com>
2024-11-20 19:50:13 +0300

    Commit Message

--- app/src/main/java/com/example/myapplication/ui/biorythms/BiorythmsFragment.java
+++ app/src/main/java/com/example/myapplication/ui/biorythms/BiorythmsFragment.java
@@ -38,10 +38,12 @@ public class BiorythmsFragment extends Fragment {
         birthDayEditText = binding.birthDayEditText;
         chart = binding.lineChart;
 
-        // Setup button to plot biorythms
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+
         binding.plotButton.setOnClickListener(v -> {
             int year = Integer.parseInt(birthYearEditText.getText().toString());
-            int month = Integer.parseInt(birthMonthEditText.getText().toString()) - 1; // Month is 0-based
+            int month = Integer.parseInt(birthMonthEditText.getText().toString()) - 1;
             int day = Integer.parseInt(birthDayEditText.getText().toString());
 
             plotBiorythms(year, month, day);
@@ -57,42 +59,36 @@ public class BiorythmsFragment extends Fragment {
         Calendar currentDate = Calendar.getInstance();
         long daysBetween = (currentDate.getTimeInMillis() - birthDate.getTimeInMillis()) / (1000 * 60 * 60 * 24); // days since birth
 
-        // Initialize ArrayLists to hold data
         ArrayList<Entry> physicalData = new ArrayList<>();
         ArrayList<Entry> emotionalData = new ArrayList<>();
         ArrayList<Entry> intellectualData = new ArrayList<>();
 
-        for (int i = 0; i < 30; i++) { // Plot biorythms for the next month (30 days)
+        for (int i = 0; i < 30; i++) {
             float physical = (float) Math.sin((2 * Math.PI * (daysBetween + i)) / 23);
             float emotional = (float) Math.sin((2 * Math.PI * (daysBetween + i)) / 28);
             float intellectual = (float) Math.sin((2 * Math.PI * (daysBetween + i)) / 33);
 
-            // Add entries for each day (x-axis: day number, y-axis: value)
             physicalData.add(new Entry(i, physical));
             emotionalData.add(new Entry(i, emotional));
             intellectualData.add(new Entry(i, intellectual));
         }
 
-        // Create LineDataSet with Entry data
         LineDataSet physicalSet = new LineDataSet(physicalData, "Physical");
         LineDataSet emotionalSet = new LineDataSet(emotionalData, "Emotional");
         LineDataSet intellectualSet = new LineDataSet(intellectualData, "Intellectual");
 
-        // Set colors for the lines (optional)
-        physicalSet.setColor(getResources().getColor(R.color.physical_color)); // Define these colors in resources
+        physicalSet.setColor(getResources().getColor(R.color.physical_color));
         emotionalSet.setColor(getResources().getColor(R.color.emotional_color));
         intellectualSet.setColor(getResources().getColor(R.color.intellectual_color));
 
-        // Add data sets to a list of ILineDataSet
         ArrayList<ILineDataSet> dataSets = new ArrayList<>();
         dataSets.add(physicalSet);
         dataSets.add(emotionalSet);
         dataSets.add(intellectualSet);
 
-        // Create LineData and set it to the chart
-        LineData data = new LineData(dataSets); // This constructor accepts a List<ILineDataSet>
+        LineData data = new LineData(dataSets);
         chart.setData(data);
-        chart.invalidate(); // Refresh the chart
+        chart.invalidate();
     }
 
     @Override
